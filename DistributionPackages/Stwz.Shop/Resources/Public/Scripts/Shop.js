7a6938be62db03b51c1cd028f887e8e2b9195187
Dominique Cinter <d.cinter@gmail.com>
2022-05-30 16:37:38 +0200

    Fix: Add conditions to shop url generation

--- DistributionPackages/Stwz.Shop/Resources/Public/Scripts/Shop.js
+++ DistributionPackages/Stwz.Shop/Resources/Public/Scripts/Shop.js
@@ -103,8 +103,8 @@
       showBtn: false,
       api_url: "https://portal.stwz-vernetzt.ch/Shop/Wizard/PlainOrderWizard.aspx",
       lang: document.documentElement.lang || "de",
-      guid: document.getElementById(localStorage.getItem("Internet")).dataset.guid,
-      iv: document.getElementById(localStorage.getItem("Internet")).dataset.iv,
+      guid: localStorage.getItem("Internet") ? document.getElementById(localStorage.getItem("Internet")).dataset.guid : "",
+      iv: localStorage.getItem("Internet") ? document.getElementById(localStorage.getItem("Internet")).dataset.iv : "",
       id: "",
       zip: "",
       city: "",
@@ -115,7 +115,7 @@
       street_key: "",
       shop_url: "",
       getShopUrl() {
-        this.guid = document.getElementById(localStorage.getItem("Internet")).dataset.guid, this.iv = document.getElementById(localStorage.getItem("Internet")).dataset.iv, this.id = document.getElementById("addressLookupForm").querySelector('[x-model="address.house_number"').value, this.zip = document.getElementById("addressLookupForm").querySelector('[x-model="address.zip_code"').value, this.city = document.getElementById("addressLookupForm").querySelector('[x-model="address.city"').value, this.street = document.getElementById("addressLookupForm").querySelector('[x-model="address.streetname"').value, this.id_key = this.iv + "2140070229", this.zip_key = this.iv + "2140070227", this.city_key = this.iv + "2140070235", this.street_key = this.iv + "2140070231", this.shop_url = urlcat(this.api_url, "/", {
+        this.guid = localStorage.getItem("Internet") ? document.getElementById(localStorage.getItem("Internet")).dataset.guid : "", this.iv = localStorage.getItem("Internet") ? document.getElementById(localStorage.getItem("Internet")).dataset.iv : "", this.id = document.getElementById("addressLookupForm").querySelector('[x-model="address.house_number"').value, this.zip = document.getElementById("addressLookupForm").querySelector('[x-model="address.zip_code"').value, this.city = document.getElementById("addressLookupForm").querySelector('[x-model="address.city"').value, this.street = document.getElementById("addressLookupForm").querySelector('[x-model="address.streetname"').value, this.id_key = this.iv + "2140070229", this.zip_key = this.iv + "2140070227", this.city_key = this.iv + "2140070235", this.street_key = this.iv + "2140070231", this.shop_url = urlcat(this.api_url, "/", {
           GUID_ID: this.guid,
           lang: this.lang,
           IV: this.iv,
