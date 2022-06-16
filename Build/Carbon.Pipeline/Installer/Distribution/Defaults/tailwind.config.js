cc315c553997678bd9df15405b7cc7093d20d5a2
Pascal <pascal@abteilung.ch>
2022-06-16 16:55:28 +0200

    upgradeRespoDesign

--- Build/Carbon.Pipeline/Installer/Distribution/Defaults/tailwind.config.js
+++ Build/Carbon.Pipeline/Installer/Distribution/Defaults/tailwind.config.js
@@ -1,5 +1,10 @@
 const content = require("./Build/Carbon.Pipeline/purge");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
